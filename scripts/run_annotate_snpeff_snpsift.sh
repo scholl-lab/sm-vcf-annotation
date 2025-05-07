c8c03591d1539a9dd448d0861a041e586b8855b4
Bernt Popp <bernt.popp@gmail.com>
2025-05-07 12:48:36 +0200

    refactor: improve script usage documentation and streamline command-line argument parsing

--- scripts/run_annotate_snpeff_snpsift.sh
+++ scripts/run_annotate_snpeff_snpsift.sh
@@ -7,23 +7,56 @@
 #SBATCH --mem=2000M
 #SBATCH --output=slurm_logs/%x-%j.log
 
-# based on:
-# https://hpc-docs.cubi.bihealth.org/best-practice/temp-files/#tmpdir-and-the-scheduler
-# https://bihealth.github.io/bih-cluster/slurm/snakemake/#custom-logging-directory
+################################################################################
+# Usage:
+#   sbatch run_annotate_snpeff_snpsift.sh [SNAKEMAKE_FILE] [CONFIG_FILE] [MAX_JOBS]
+#
+#   - SNAKEMAKE_FILE:   Path to the Snakemake workflow file
+#                       (default: "snakemake/annotate_snpeff_snpsift.smk")
+#   - CONFIG_FILE:      Path to a Snakemake config file (default: "config.yaml")
+#   - MAX_JOBS:         Number of Snakemake jobs (in parallel) to use (default: 50)
+#
+################################################################################
 
+# ------------------------------------------------------------------------------
+# 1) Parse command-line arguments with defaults
+# ------------------------------------------------------------------------------
+SNAKEMAKE_FILE=${1:-"snakemake/annotate_snpeff_snpsift.smk"}
+CONFIG_FILE=${2:-"config.yaml"}
+MAX_JOBS=${3:-50}
+
+# ------------------------------------------------------------------------------
+# 2) HPC environment setup
+# ------------------------------------------------------------------------------
 # First, point TMPDIR to the scratch in your home as mktemp will use this
-export TMPDIR=$HOME/scratch/tmp
+export TMPDIR="$HOME/scratch/tmp"
 # Second, create another unique temporary directory within this directory
 export TMPDIR=$(mktemp -d)
 # Finally, setup the cleanup trap
 trap "rm -rf $TMPDIR" EXIT
 
+# Create the slurm_logs directory if it doesn't exist
 mkdir -p slurm_logs
-export SBATCH_DEFAULTS=" --output=slurm_logs/%x-%j.log"
 
-# Get the number of jobs from the command line argument, default to 50 if not provided
-JOBS=${1:-50}
+# Export default SBATCH outputs
+export SBATCH_DEFAULTS="--output=slurm_logs/%x-%j.log"
+
+echo "DEBUG: Running annotation pipeline with:"
+echo "       Snakefile:    $SNAKEMAKE_FILE"
+echo "       Config file:  $CONFIG_FILE"
+echo "       Max jobs:     $MAX_JOBS"
+echo "       TMPDIR:       $TMPDIR"
 
 date
-srun snakemake -s snakemake/annotate_snpeff_snpsift.smk --use-conda --profile=cubi-v1 -j$JOBS
+
+# ------------------------------------------------------------------------------
+# 3) Run Snakemake (e.g. via srun), using conda environment & cluster profile
+# ------------------------------------------------------------------------------
+srun snakemake \
+    -s "$SNAKEMAKE_FILE" \
+    --use-conda \
+    --profile=cubi-v1 \
+    -j "$MAX_JOBS" \
+    --configfile "$CONFIG_FILE"
+
 date
